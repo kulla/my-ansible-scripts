c7fe83a32d9589e22d73cd8e4f21e1cbc0498bfa
Stephan Kulla <git.mail@kulla.me>
2019-08-15 15:07:05 +0200

    asdf: Set ASDF_DATA_DIR in configuration

--- roles/asdf/templates/configure_asdf.sh
+++ roles/asdf/templates/configure_asdf.sh
@@ -1,2 +1,4 @@
-. {{ asdf_install_dir }}/asdf.sh
-. {{ asdf_install_dir }}/completions/asdf.bash
+export ASDF_DATA_DIR='{{ asdf_install_dir }}'
+
+. '{{ asdf_install_dir }}/asdf.sh'
+. '{{ asdf_install_dir }}/completions/asdf.bash'
