3790e5fb1412d00091c71bb1f172bf5c57e9eedb
Stephan Kulla <git.mail@kulla.me>
2019-11-30 14:55:53 +0100

    Add onlyonly playbook to run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 sudo ansible-galaxy install -r requirements.yml
 sudo ansible-playbook "$@" -i inventory \
-	--vault-password-file `pwd`/.vault_pass.txt
+	--vault-password-file `pwd`/.vault_pass.txt main_computer.yml
